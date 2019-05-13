c588b1f64ffcf5ec30acfc431e83485b10a7aa97
jacquline <jacqulinewangu@gmail.com>
2019-05-13 09:56:18 +0300

    added details of tha account as facebook username and password

--- test.py
+++ test.py
@@ -18,3 +18,12 @@ def setUp(self):
         Set up method to run before each test cases.
         '''
         self.new_user = User("jacqarta1167@gmail.com","0707518860","Jacquline","Wangu") # create user object
+
+        def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.account,"Facebook")
+        self.assertEqual(self.new_user.username,"MichiganMaine")
+        self.assertEqual(self.new_user.password,"wawoodz")
