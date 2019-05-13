54cd2b96a245cf15d12f3bfd1859cce9d715476e
jacquline <jacqulinewangu@gmail.com>
2019-05-13 07:50:58 +0300

    corrected my errors on indentation in the user.py file.

--- user.py
+++ user.py
@@ -3,13 +3,12 @@ import pyperclip
 
 class User:
 
+    user_list = []  # Empty user list
 
-    user_list = [] # Empty user list
+    def __init__(self, email, number, first_name, last_name, password):
 
-    def __init__(self, email, number,first_name,last_name,password):
-
-         self.email = email
-         self.phone_number = number
+        self.email = email
+        self.phone_number = number
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
