a74804c638c51a9afddae53ed2f9e959e18c5e65
jacquline <jacqulinewangu@gmail.com>
2019-05-13 09:46:26 +0300

    imported the unittest module and user class in the test.py file

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import pyperclip # Importing the unittest module
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Argumentss:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
